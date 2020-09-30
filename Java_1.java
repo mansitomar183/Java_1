7bd64689a23c12169592ce9ba61fc5ef32d96388
Mansi <myselfmansi18@gmail.com>
2020-09-30 22:46:41 +0530

    first java program

---
+++ Java_1.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+class Java_1{
+public static void main(String[] args){
+scanner sc=new scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=a+b;
+System.out.print(c);
+}
+
+
+}
